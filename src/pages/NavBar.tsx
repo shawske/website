2847b2e2a044edad81e8c7193398666478e0627c
shawske <rjshaw@umich.edu>
2024-01-16 11:40:47 -0500

    production

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 const NavBar = () => {
   const navigate = useNavigate();
